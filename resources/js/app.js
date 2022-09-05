f458dc0b36d1aa000cf4decfac43782d57a45040
Lukasz Dutkiewicz <l_dutkiewicz@o2.pl>
2022-09-05 22:04:47 +0200

    Vue with errors

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,30 @@
-require('./bootstrap');
+import Vue from 'vue'
+import mainVue from "./components/mainVue"
+import routes from "./routes"
+import VueResource from "vue-resource"
+import Vuetify from '../plugins/vuetify'
+import VueRouter from 'vue-router'
+
+
+window.Vue = require('vue').default;
+
+Vue.use(VueRouter);
+Vue.use(VueResource);
+
+
+const router = new VueRouter(routes);
+
+const app = new Vue({
+    vuetify: Vuetify,
+
+    el: '#app',
+    router: router,
+    components: {
+        mainVue
+    }
+});
+
+
+
+
+export const bus = new Vue()
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,17 +1,15 @@
 const mix = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
- | file for the application as well as bundling up all the JS files.
- |
- */
+var CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
+var VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin');
+
+var webpackConfig = {
+    plugins: [
+        new VuetifyLoaderPlugin(),
+        CaseSensitivePathsPlugin()
+    ]
+}
 
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+    .vue()
+    .sass('resources/sass/app.scss', 'public/css');
